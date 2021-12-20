b274a06ac689eb486b2ff0a443208e18bc49fb60
cheng521521 <15503679582@163.com>
2021-12-20 22:35:28 +0800

    “first commit”

---
+++ src/main/java/com/cpx/JavaErrorBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.cpx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaErrorBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaErrorBusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpx/JavaErrorBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaErrorBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
